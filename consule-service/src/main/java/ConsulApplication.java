6d9fc667f07f9123f09048bdcbd0468c4d2a1d48
bwzheng <zheng.biwen@puscene.com>
2018-11-27 11:26:18 +0800

    update

--- consule-service/src/main/java/ConsulApplication.java
+++ consule-service/src/main/java/ConsulApplication.java
@@ -1,12 +1,14 @@
 import com.zbw.config.TestConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan({"com.zbw.controller"})
+@EnableConfigurationProperties({TestConfig.class})
 public class ConsulApplication {
 
     public static void main(String[] args) {

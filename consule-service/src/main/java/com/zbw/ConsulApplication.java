930090311b703302efc9388ec85decf9b5d39e24
bwzheng <zheng.biwen@puscenc.com>
2019-01-08 18:35:19 +0800

    update

--- consule-service/src/main/java/com/zbw/ConsulApplication.java
+++ consule-service/src/main/java/com/zbw/ConsulApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-//@ComponentScan({"com.zbw"})
+@ComponentScan({"com.zbw"})
 @EnableConfigurationProperties
+@EnableScheduling
 public class ConsulApplication {
 
     public static void main(String[] args) {
